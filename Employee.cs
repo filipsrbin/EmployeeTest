61947efb721247fd3cebac9690e70522b9014e33
filip <filipsrbin@hotmail.com>
2018-01-29 20:25:50 +0100

    inital commit

---
+++ Employee.cs
@@ -0,0 +1,11 @@
+public class Employee {
+
+    public Employee(string name, string EmployedAt, int id) {
+        Name = name;
+        EmployedAt = EmployedAt;
+        EmployeeId = id;
+    }
+    string Name {get; set;}
+    string EmployedAt {get; set;}
+    int EmployeeId {get; set;}
+}
